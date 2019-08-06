4172baeef9e545159c10c2ec50ffafdd3a46fcf7
binshuangli <9728557+binshuangli@users.noreply.github.com>
2019-08-06 16:31:01 -0700

    Update Simulate21.py
    Updated the Simualte21 class. Use it to do the actual simulation

--- Simulate21.py
+++ Simulate21.py
@@ -1,49 +1,57 @@
 class Simulate21:
+    def __init__(self):
+        cards = {1:4,2:4,3:4,4:4,5:4,6:4,7:4,8:4,9:4,10:16}
+        tmp = []
+        for k,v in cards.items():
+            tmp.append([k]*v)
+        cards_list = [card for i in tmp for card in i]
+        self.cards = cards_list
+        
     def dealer_result(self):
-        cards = {1:4,2:4,3:4,4:4,5:4,6:4,7:4,8:4,9:4,10:16}
-        tmp = []
-        for k,v in cards.items():
-            tmp.append([k]*v)
-        cards_list = [card for i in tmp for card in i]
-        current = random.choice(cards_list)
-        cards_list.remove(current)
+        current = random.choice(self.cards)
+        self.cards.remove(current)
         current_sum = current
         while current_sum<=21:
-            current = random.choice(cards_list)
+            current = random.choice(self.cards)
             current_sum+=current
-            cards_list.remove(current)
-            if current_sum>=17:
-                break
-        return current_sum
-    
-    def dealer_result_observed(self,observed):
-        cards = {1:4,2:4,3:4,4:4,5:4,6:4,7:4,8:4,9:4,10:16}
-        tmp = []
-        for k,v in cards.items():
-            tmp.append([k]*v)
-        cards_list = [card for i in tmp for card in i]
-        current = observed
-        cards_list.remove(current)
-        current_sum = current
-        while current_sum<=21:
-            current = random.choice(cards_list)
-            current_sum+=current
-            cards_list.remove(current)
+            self.cards.remove(current)
             if current_sum>=17:
                 break
         return current_sum
         
+    def dealer_result_observed(self,observed):
+        current = observed
+        self.cards.remove(current)
+        current_sum = current
+        while current_sum<=21:
+            current = random.choice(self.cards)
+            current_sum+=current
+            self.cards.remove(current)
+            if current_sum>=17:
+                break
+        return current_sum
     
-    def simulation(self,iterations,observed = 0):
-        legal_results = []
-        lost_count = 0
-        for i in range(iterations):
-            if observed !=0:        
-                simulation_result = self.dealer_result_observed(observed)
-            else:
-                simulation_result = self.dealer_result()
-            if simulation_result<=21:
-                legal_results.append(simulation_result)
-            else:
-                lost_count+=1
-        return np.mean(legal_results),lost_count, lost_count/iterations, (1-lost_count/iterations)*np.mean(legal_results)
+    def player_cards(self):
+        card1 = random.choice(self.cards)
+        self.cards.remove(card1)
+        card2 = random.choice(self.cards)
+        self.cards.remove(card2)
+        total = card1 + card2
+        if card1 == 1 or card2 == 1:
+            total = total+10
+        return card1,card2,total
+        
+def simulation(iterations,observed = 0):
+    legal_results = []
+    lost_count = 0
+    for i in range(iterations):
+        sim=Simulate21()
+        if observed !=0:        
+            simulation_result = sim.dealer_result_observed(observed)
+        else:
+            simulation_result = sim.dealer_result()
+        if simulation_result<=21:
+            legal_results.append(simulation_result)
+        else:
+            lost_count+=1
+    return np.mean(legal_results),lost_count, lost_count/iterations, (1-lost_count/iterations)*np.mean(legal_results)
